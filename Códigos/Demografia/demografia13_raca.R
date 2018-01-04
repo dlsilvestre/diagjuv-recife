fd363664fb3e78f7b4e3604d666b84572d200186
claudioalvesmonteiro <claudiomonteiro@protonmail.com>
2018-01-04 13:54:22 -0300

    commit sb 04-01-18.2

---
+++ Códigos/Demografia/demografia13_raca.R
@@ -0,0 +1,33 @@
+#================================================#
+# JUVENTUDE NA CIDADE DO RECIFE                  #
+# Demografia 1.3 - Raça e Cor                    #
+#================================================#
+# Recife - Pernambuco - Brasil                   #
+# Prefeitura da Cidade do Recife                 #
+#------------------------------------------------#
+# Secretaria Executiva de Juventude              #
+#------------------------------------------------#
+# Claudio A. Monteiro                            #
+# claudiomonteiro@gmail.com                      #
+#------------------------------------------------#
+# Qualquer duvida contate o desenvolvedor        #
+# #UseSoftwareLivre                              #
+#------------------------------------------------#
+
+# instalar pacotes necessarios
+# install.packages(c("readxl"))
+
+# carregar pacotes
+library(readxl)
+
+#----carregar banco de dados----#
+pop_idaderaca_2010 <- read_excel("Dados Originais/Demografia/pop_idaderaca_2010.xlsx")
+
+
+
+
+
+
+
+
+
