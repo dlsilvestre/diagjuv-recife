f8446500f5a8ffe0751887908c2edadbaf2a1ac8
MPCP- UFPE <mpcp@iMac-de-MPCP.local>
2018-01-05 13:13:42 -0300

    commit la

--- Códigos/Demografia/demografia13_raca.R
+++ Códigos/Demografia/demografia13_raca.R
@@ -74,9 +74,23 @@ pop_idaderaca_2010 <- rbind(pop_idaderaca_amarela,
 # remover 
 pop_idaderaca_2010 <- pop_idaderaca_2010[,-c(4:6)]
 
-# transformar de long em wide
+#----transformar banco e criar variaveis----#
+
+# transformar de long para wide
 pop_idaderaca_2010 <- reshape(pop_idaderaca_2010, idvar = "localidade", 
               timevar = "raca_cor", direction = "wide")
+
+# proporcao de jovens pretos do total de jovens
+pop_idaderaca_2010 <- mutate(pop_idaderaca_2010, 
+                             prop_preto_jovem = (pop_jovem.Preta / pop_jovem.Total)*100)
+
+# proporcao de jovens pardos do total de jovens
+pop_idaderaca_2010 <- mutate(pop_idaderaca_2010, 
+                             prop_pardo_jovem = (pop_jovem.Parda / pop_jovem.Total)*100)
+
+# proporcao de jovens indigenas do total de jovens
+pop_idaderaca_2010 <- mutate(pop_idaderaca_2010, 
+                             prop_indigena_jovem = (pop_jovem.Indígena / pop_jovem.Total)*100)
 
 #===============================
 # VISUALIZACAO GRAFICA 
@@ -98,17 +112,92 @@ theme_arretado<- function (base_size = 12, base_family = "") {
           axis.line = element_line(size = 1, colour = "grey70"))
 }
 
-#----------------------------------------------#
-#         BAIRROS DA CIDADE DO RECIFE
-#----------------------------------------------#
+#----------------------------------------#
+#  MAPA RACA/COR DOS JOVENS DO RECIFE
+#----------------------------------------#
 
-reshape(mydf, direction = "wide", 
-        idvar = c("hash", "age", "sex"), 
-        timevar = "color")
+# remover outros niveis
 
 
+#=================
+# Raca/cor preta
+shape_recife <- shapefile("Dados Originais/Geodata/Bairros.shp")
 
+#
+#
+shape <- shape_recife
+data <- pop_idaderaca_2010
 
+#==== MAP FUNCTION ====#
+
+# mapa.funcao <- function(shape, data) { 
+  library(ggrepel)
+  
+  # function to create merge string based on similarity
+  best_match= function(string_vector,string_replacement){
+    s<-string_replacement %>% 
+      purrr::map_int(~{
+        .x %>% 
+          RecordLinkage::levenshteinSim(string_vector) %>%
+          match(max(.),.)
+      })
+    string_vector[s] = string_replacement
+    return(string_vector)
+  }
+  
+  data$EBAIRRNOME = data$localidade
+  data$EBAIRRNOME = toupper(data$EBAIRRNOME)
+  data$EBAIRRNOME = stri_trans_general(data$EBAIRRNOME , "Latin-ASCII")
+  data$EBAIRRNOME = best_match(data$EBAIRRNOME, shape$EBAIRRNOME)
+  
+  # merge data with shapefile
+  shp_data <- merge(shape, data, by = "EBAIRRNOME")
+  
+  # definir labels no mapa
+  shp_data <- shp_data[order(shp_data$pop_jovem.Indígena),]
+  shp_data$bairros_detasq <- 1
+  shp_data$bairros_detasq[1:3] <- ""
+  shp_data$bairros_detasq[c(length(shp_data)-2):c(length(shp_data))] <- ""
+  
+  shp_data$bairros_detasq <- with(shp_data, paste0(shp_data$bairros_detasq, shp_data$localidade))
+  shp_data$bairros_detasq_cod <- grepl(shp_data$bairros_detasq, pattern = "1")
+  shp_data$bairros_detasq[shp_data$bairros_detasq_cod == TRUE ] <- ""
+  
+  # tranformar shapefile em polygonsdataframe
+  data_fortity <- fortify(shp_data, region = "localidade")
+  localidade <- shp_data@data$localidade
+  
+  # extrair centroides dos poligonos
+  centroids.df <- as.data.frame(coordinates(shp_data))
+  names(centroids.df) <- c("Longitude", "Latitude")  #more sensible column localidades
+  
+  # This shapefile contained population data, let's plot it.
+  variavel <- shp_data@data$pop_jovem.Indígena
+  nomes_centroides <- shp_data$bairros_detasq
+  
+  map_dataframe <- data.frame(localidade, variavel, centroids.df, nomes_centroides)
+  
+  plot <- ggplot(data = map_dataframe, aes(map_id = localidade)) + 
+    geom_map(aes(fill = variavel), colour= grey(0.8), map = data_fortity) +
+    expand_limits(x = data_fortity$long, y = data_fortity$lat) + 
+    scale_fill_gradient(name = "Porcentagem", low="lightgreen", high= "darkblue")+
+    geom_label_repel(aes(label = nomes_centroides, x = Longitude, y = Latitude),
+                     size = 5, color = "black") + #add labels at centroids
+  coord_fixed(1) +
+    labs(title = "População Absoluta de Jovens Indígenas")+
+    theme_nothing(legend = T)+
+    theme(legend.key.size = unit(1.1, "cm"),
+          legend.text = element_text(size = 14, hjust = 3, vjust = 3),
+          legend.title = element_text(size = 14),
+          title = element_text(colour="black",size=18,angle=0,face="bold"))
+  
+  print(plot)
+  
+  ggsave("jovens_indigenas_POP.png",  width = 8.5, height = 11, units = "in")
+#  return(plot)
+# }
+
+mapa.funcao(shape_recife, demojovem_2000)
 
 
 
