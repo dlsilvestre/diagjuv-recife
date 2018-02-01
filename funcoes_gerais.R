796cd9eb2864f57a3a81a7eee9ab504c5397ba84
MPCP- UFPE <mpcp@iMac-de-MPCP.local>
2018-02-01 13:00:22 -0300

    commit funcoes gerais

--- funcoes_gerais.R
+++ funcoes_gerais.R
@@ -15,3 +15,19 @@
 # #UseSoftwareLivre                                  #
 #----------------------------------------------------#
 
+
+# tema ggplot2 para graficos
+tema_massa <- function (base_size = 12, base_family = "") {
+  theme_minimal(base_size = base_size, base_family = base_family) %+replace% 
+    theme(axis.text.x = element_text(colour= "black",size=11,hjust=.5,vjust=.5,face="plain"),
+          axis.text.y = element_text(colour="black",size=11,angle=0,hjust=1,vjust=0,face="plain"), 
+          axis.title.x = element_text(colour="black",size=12,angle=0,hjust=.5,vjust=0,face="plain"),
+          axis.title.y = element_text(colour="black",size=12,angle=90,hjust=0.5,vjust=0.6,face="plain"),
+          title = element_text(colour="black",size=14,angle=0,hjust=.5,vjust=.5,face="bold"),
+          panel.grid.major = element_line(colour = grey(0.85)), 
+          panel.grid.minor = element_line(colour = grey(1)),
+          legend.key.size = unit(9, "mm"),
+          legend.text = element_text(size = 9, hjust = 3, vjust = 3),
+          legend.title = element_text(size = 9),
+          axis.line = element_line(size = 1, colour = "grey70"))
+}
